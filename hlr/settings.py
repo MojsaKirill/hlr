6569f5b707df20c806c221cd2410532d2a014636
Kirill <kiril_moisa@senla.eu>
2021-07-02 19:38:47 +0300

    edit settings.py

--- hlr/settings.py
+++ hlr/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'v0khoc-pq*t+7)3@%+@khqx8&ba&4xbv!@6v+fi0a(b-ylkw9g'
 DEBUG = True
 
 ALLOWED_HOSTS = ['chekerbase.ru']
-
 
 # Application definition
 
@@ -43,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hlr.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +114,6 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'check_phone.User'
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_DIRS = [
@@ -129,6 +124,8 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 GOOGLE_RECAPTCHA_SECRET_KEY = '6LdH4m4bAAAAAMtCmscYJjUWobmAl9BFyzYghZKc'
 LOGOUT_REDIRECT_URL = '/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ROBOKASSA_LOGIN=
 # ROBOKASSA_PASSWORD1=
